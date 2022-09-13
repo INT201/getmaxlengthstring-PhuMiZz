05b89d93e4ef295d012a83e17bcfca587cef907b
Siripoom <siripoom.kusonsong@mail.kmutt.ac.th>
2022-09-13 20:07:45 +0700

    code : finish

--- getMaxLengthString.js
+++ getMaxLengthString.js
@@ -1,6 +1,22 @@
-const { template } = require('@babel/core')
+const { template } = require("@babel/core");
 
 function getMaxLengthString(arrayOfString) {
   //code here
+  let longestStr = arrayOfString[0];
+  if (arrayOfString === null || arrayOfString === undefined) return undefined;
+
+  for (const maxi of arrayOfString) {
+    if (maxi.length >= longestStr[0].length) {
+      if (maxi.length === longestStr[0].length) {
+        longestStr.push(maxi);
+      } else {
+        longestStr = [maxi];
+      }
+    }
+  }
+  return longestStr;
 }
-module.exports = getMaxLengthString
+console.log(
+  getMaxLengthString(["hello", "hi", "hey", "Good Evening", "Good Morning"])
+);
+module.exports = getMaxLengthString;
